83fd58f0f63002843610da6442eb360297eff5af
Joshua de Gier <joshua@pendo.nl>
2024-10-28 10:50:44 +0100

    wip

--- app.ts
+++ app.ts
@@ -2,12 +2,16 @@
 
 import Homey from 'homey';
 import { RingApi } from 'ring-client-api'
+import { readFile, writeFile } from 'fs'
+import { promisify } from 'util'
 
 module.exports = class MyApp extends Homey.App {
   async onInit() {
     this.log('RingAlarmState has been initialized');
 
-    await this.updateRingAlarmState()
+    const ringAlarmState = await this.updateRingAlarmState();
+
+    this.homey.settings.set('ring_alarm_state', ringAlarmState)
 
     setInterval(() => this.updateRingAlarmState(), 10000)
   }
@@ -19,18 +23,41 @@ module.exports = class MyApp extends Homey.App {
 
     ringApi.onRefreshTokenUpdated.subscribe(
         async ({ newRefreshToken, oldRefreshToken }) => {
-          // console.log("Refresh Token Updated: ", newRefreshToken);
+          // console.log('Refresh Token Updated: ', newRefreshToken)
+
+          // If you are implementing a project that use `ring-client-api`, you should subscribe to onRefreshTokenUpdated and update your config each time it fires an event
+          // Here is an example using a .env file for configuration
+          /*if (!oldRefreshToken) {
+            return
+          }
+
+          const currentConfig = await promisify(readFile)('../env.json'),
+              updatedConfig = currentConfig
+                  .toString()
+                  .replace(oldRefreshToken, newRefreshToken)
+
+          await promisify(writeFile)('../env.json', updatedConfig)*/
         }
     );
+
+    const ringArmedStateChanged = this.homey.flow.getTriggerCard('ring-armed-state-changed');
 
     // const devices = await ringApi.fetchRingDevices();
     const locations = await ringApi.getLocations();
     const home = locations.find((l) => l.locationDetails.location_id == Homey.env.LOCATION_ID);
 
     if(home) {
-      const state = await home.getAlarmMode();
+        const state = await home.getAlarmMode();
 
-      this.log(state);
+        if(this.homey.settings.get('ring_alarm_state') !== state) {
+            this.homey.settings.set('ring_alarm_state', state)
+
+            await ringArmedStateChanged.trigger({
+                'armed-state': state
+            });
+
+            console.log('state changed to ' + state);
+        }
     }
   }
 
